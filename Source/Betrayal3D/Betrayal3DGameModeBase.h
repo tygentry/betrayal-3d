13b799999e0ee3e88ace4d4daba37d4214dc10b5
Tyler Gentry <tygentry00@gmail.com>
2019-08-02 15:27:49 -0400

    Initial commit of UProject

---
+++ Source/Betrayal3D/Betrayal3DGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Betrayal3DGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BETRAYAL3D_API ABetrayal3DGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
